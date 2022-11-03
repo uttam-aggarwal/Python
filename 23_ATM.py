76e6746d6d99cbdb30329d22a60ae50b0c3b99a1
uttam-aggarwal <115714793+uttam-aggarwal@users.noreply.github.com>
2022-11-03 19:13:37 +0530

    projects

--- 23_ATM.py
+++ 23_ATM.py
@@ -11,20 +11,33 @@ print("CASH AVAILABLE IN YOUR ACCOUNT IS $25000 ")
 BALANCE=25000
 while True:
     a=int(input('''PRESS 1 TO WITHDRAW CASH
-                   PRESS 2 TO DEPOSIT  CASH
-                   PRESS 3 TO KNOW YOUR CURRENT BALANCE
-                   PRESS 4 TO EXIT
-                   ENTER: '''))
+PRESS 2 TO DEPOSIT  CASH
+PRESS 3 TO KNOW YOUR CURRENT BALANCE
+PRESS 4 TO EXIT
+ENTER: '''))
     if a==1:
         W_AMOUNT=int(input("ENTER THE AMOUNT TO WITHRAW IN MULTIPLE OF 500: "))
         if BALANCE>=W_AMOUNT:
             if W_AMOUNT%500==0:
-                print(AMOUNT, "HAS BEEN WITHDRAW SUCCESSFULLY")
-                print("CURRENT BALANCE IS ",BALANCE-W_AMOUNT)
+                print(W_AMOUNT, "HAS BEEN WITHDRAWN SUCCESSFULLY")
+                BALANCE-=W_AMOUNT
+                print("CURRENT BALANCE IS ",BALANCE)
+                
             else:
                 print("INAPPOROPIRATE ENTRY")
         else:
             print("INSUFFICENT BALANCE")
-    if a==2:
+    elif a==2:
         D_AMOUNT=int(input("ENTER THE AMOUNT TO DEPOSIT: "))
-        print("")
+        BALANCE+=D_AMOUNT
+        print("CURRENT BALANCE IS ",BALANCE)
+    elif a==3:
+        print("CURRENT BALANCE IS ",BALANCE)
+        if BALANCE<5000:
+            print("LOW BALANCE")
+    elif a==4:
+        print('''THANKS FOR USING ATM
+        ---- SUCCESSFULLY EXITED ----''')
+        break
+    
+    
