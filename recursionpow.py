9d3f13a5efa573a26d4c6baf36ebf64b6f58f6ae
uttam-aggarwal <115714793+uttam-aggarwal@users.noreply.github.com>
2022-12-05 22:09:34 +0530

    __

--- recursionpow.py
+++ recursionpow.py
@@ -4,7 +4,7 @@ power=int(input("enter the number: "))
 def pow(a,b):
     if b==0:
         return 1
-    powm1=pow(a,b-1)#here is recursion happening
+    powm1=pow(a,b-1)#here is recursion happen
     powm=powm1*a
     return powm
 print(pow(num,power))
